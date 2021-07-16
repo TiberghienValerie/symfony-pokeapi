f2b095af2d14ba3cbdd74b7b22107fceec6eafc6
TiberghienValerie <tiberghien.valerie@wanadoo.fr>
2021-07-16 14:43:27 +0200

    Set entity et relation

---
+++ src/Entity/Attack.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\AttackRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=AttackRepository::class)
+ */
+class Attack
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $pokeapiId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $pp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $accuracy;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $power;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=type::class, inversedBy="attacks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPokeapiId(): ?int
+    {
+        return $this->pokeapiId;
+    }
+
+    public function setPokeapiId(int $pokeapiId): self
+    {
+        $this->pokeapiId = $pokeapiId;
+
+        return $this;
+    }
+
+    public function getPp(): ?int
+    {
+        return $this->pp;
+    }
+
+    public function setPp(int $pp): self
+    {
+        $this->pp = $pp;
+
+        return $this;
+    }
+
+    public function getAccuracy(): ?int
+    {
+        return $this->accuracy;
+    }
+
+    public function setAccuracy(int $accuracy): self
+    {
+        $this->accuracy = $accuracy;
+
+        return $this;
+    }
+
+    public function getPower(): ?int
+    {
+        return $this->power;
+    }
+
+    public function setPower(int $power): self
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    public function getType(): ?type
+    {
+        return $this->type;
+    }
+
+    public function setType(?type $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PokemonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PokemonRepository::class)
+ */
+class Pokemon
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $pokeapiId;
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     */
+    private $height;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $weight;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $baseExperience;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $orderPoke;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=type::class, inversedBy="pokemon")
+     */
+    private $type;
+
+    public function __construct()
+    {
+        $this->type = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPokeapiId(): ?int
+    {
+        return $this->pokeapiId;
+    }
+
+    public function setPokeapiId(int $pokeapiId): self
+    {
+        $this->pokeapiId = $pokeapiId;
+
+        return $this;
+    }
+
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getBaseExperience(): ?int
+    {
+        return $this->baseExperience;
+    }
+
+    public function setBaseExperience(int $baseExperience): self
+    {
+        $this->baseExperience = $baseExperience;
+
+        return $this;
+    }
+
+    public function getOrderPoke(): ?int
+    {
+        return $this->orderPoke;
+    }
+
+    public function setOrderPoke(int $orderPoke): self
+    {
+        $this->orderPoke = $orderPoke;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|type[]
+     */
+    public function getType(): Collection
+    {
+        return $this->type;
+    }
+
+    public function addType(type $type): self
+    {
+        if (!$this->type->contains($type)) {
+            $this->type[] = $type;
+        }
+
+        return $this;
+    }
+
+    public function removeType(type $type): self
+    {
+        $this->type->removeElement($type);
+
+        return $this;
+    }
+}
